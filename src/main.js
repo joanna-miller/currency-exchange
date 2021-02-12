36c8d2c5b7619f5b82cd2c0cf6d88b859816dadc
Jo <joannadawnmiller@gmail.com>
2021-02-12 13:26:54 -0500

    can no longer access exchange rates from user input

--- src/main.js
+++ src/main.js
@@ -4,21 +4,26 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import ExchangeService from "./js/exchange-service.js";
 
-async function getExchangeRate(forex) {
-  const response = await ExchangeService.exchangeRate();
+async function getExchangeRate(forex, dollar) {
+  let response = await ExchangeService.exchangeRate();
+  console.log(dollar);
+  console.log(forex);
   console.log(response.conversion_rates.forex);
+  
 }
 
+function clearFields() {
+  $("#dollar-amount").val("");
+  $("#currency-convert").val("");
+}
 
 $(document).ready(function() {
-  
   $("#exchange-form").submit(function(event) {
     event.preventDefault();
     let dollarAmount = parseInt($("#dollar-amount").val());
     let currencyConvert = $("#currency-convert").val();
-    console.log(currencyConvert);
-    console.log(dollarAmount);
-    getExchangeRate(currencyConvert);
+    getExchangeRate(currencyConvert, dollarAmount);
+    clearFields();
   })
   
 });
