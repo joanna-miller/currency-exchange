464964b784e4a8a48dd2253a70889ef325631454
Jo <joannadawnmiller@gmail.com>
2021-02-12 16:05:11 -0500

    add conditional returns to user for invalid entries

--- src/main.js
+++ src/main.js
@@ -4,15 +4,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import ExchangeService from "./js/exchange-service.js";
 
-async function getExchangeRate(forex) {
+async function getExchangeRate(dollar, forex) {
   let response = await ExchangeService.exchangeRate();
-  const exchangeRate = (response.conversion_rates[forex]);
-  console.log(exchangeRate);
+  const exchangeRate = (dollar * (response.conversion_rates[forex])).toFixed(2);
+  if (isNaN(dollar)) {
+    $("#result").text(`Please enter a valid USD amount`);
+  } else if (isNaN(exchangeRate)) {
+    $("#result").text(`Please enter a valid exchange currency`);
+  } else {
+    $("#result").text(`$${dollar} is worth ${exchangeRate} ${forex}`);
+  }
 }
 
 function clearFields() {
   $("#dollar-amount").val("");
   $("#currency-convert").val("");
+  $("#result").empty();
 }
 
 $(document).ready(function() {
@@ -20,9 +27,7 @@ $(document).ready(function() {
     event.preventDefault();
     let dollarAmount = parseInt($("#dollar-amount").val());
     let currencyConvert = $("#currency-convert").val();
-    console.log(dollarAmount);
-    console.log(currencyConvert);
-    getExchangeRate(currencyConvert);
+    getExchangeRate(dollarAmount, currencyConvert);
     clearFields();
   });
   
